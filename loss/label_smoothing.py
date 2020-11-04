6cdad275ac06dade7d45ab3aa5444d5f562b4fa3
cxqj <43702702+cxqj@users.noreply.github.com>
2020-11-04 14:54:36 +0800

    Update label_smoothing.py

--- loss/label_smoothing.py
+++ loss/label_smoothing.py
@@ -19,11 +19,12 @@ class LabelSmoothing(nn.Module):
         # prior (uniform)
         dist = self.smoothing * torch.ones_like(pred) / (V - 2)  # (B * Seq_Len, Vocab_size)
         # add smoothed ground-truth to prior (args: dim, index, src (value))
+        # 第一个1表示沿着第一个维度，target.unsqueeze(-1)表示target对应的索引，1-self.smoothing表示填充的数字
         dist.scatter_(1, target.unsqueeze(-1).long(), 1-self.smoothing)  # 将dist中target对应的位置置为0.3    PyTorch 中，一般函数加下划线代表直接在原来的 Tensor 上修改
         # make the padding token to have zero probability 
         dist[:, self.pad_idx] = 0
         # ?? mask: 1 if target == pad_idx; 0 otherwise
-        mask = torch.nonzero(target == self.pad_idx)  # target中pad_idx所在的索引
+        mask = torch.nonzero(target == self.pad_idx)  # target中pad_idx所在的索引，将pad_idx的dist置为0
         
         if mask.sum() > 0 and len(mask) > 0:
             # dim, index, val
